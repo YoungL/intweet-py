1a6fafaf42851512488275a8e24b4fbbed621c3a
Leon Young <leon.young@rackspace.co.uk>
2018-04-08 17:06:25 +0100

    General UI Improvements and Fixes

--- intweet/config.py
+++ intweet/config.py
@@ -1,6 +1,6 @@
 DEBUG = True
 DB_URL = "mysql+pymysql://root:Pa55word@127.0.0.1/intweet"
-CONFIG = {"site_name": "Intweet"}
+CONFIG = {"site_name": "Intweet", "site_url": "http://localhost:5000"}
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
 ACCESS_TOKEN = ""
