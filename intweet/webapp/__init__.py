f27d3677e6d9810f4320d051421c92bdafd25cf5
Leon Young <leon.young@rackspace.co.uk>
2018-04-12 19:45:03 +0100

    jinja2 datetime to unix timestamp filter

--- intweet/webapp/__init__.py
+++ intweet/webapp/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from intweet.webapp.config import SECRET_KEY
 from intweet.webapp.routes import bp
+import time
+
 
 webapp = Flask(__name__)
 webapp.secret_key = SECRET_KEY
 webapp.register_blueprint(bp)
+
+
+def format_unix_timestamp(dtime):
+    return int(time.mktime(dtime.timetuple()))
+
+
+webapp.jinja_env.filters['unixtime'] = format_unix_timestamp
