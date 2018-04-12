c71d10a1831c9e7840050b2a52f59c4fdedad39f
Leon Young <leon.young@rackspace.co.uk>
2018-04-12 19:45:43 +0100

    Initial tweet analytics

--- intweet/webapp/routes.py
+++ intweet/webapp/routes.py
@@ -10,6 +10,8 @@ import validators
 import re
 from sqlalchemy import func, and_
 from sqlalchemy.orm.exc import NoResultFound
+import datetime
+from collections import OrderedDict
 
 
 bp = Blueprint('routes', __name__)
@@ -28,11 +30,19 @@ def home():
             'fullname': session.get('name'),
             'email': session.get('email')
         }
-        return render_template('user_home.html', global_config=CONFIG,
-                               local_config=local_config, userdata=userdata)
+        return render_template(
+            'user_home.html',
+            global_config=CONFIG,
+            local_config=local_config,
+            menu_rules=generate_menu_items(),
+            userdata=userdata
+        )
     else:
-        return render_template('home.html', global_config=CONFIG,
-                               local_config=local_config)
+        return render_template(
+            'home.html',
+            global_config=CONFIG,
+            local_config=local_config
+        )
 
 
 @bp.route('/logout')
@@ -151,6 +161,95 @@ def register():
     )
 
 
+@bp.route('/analytics/show/<path:rule>', methods=['POST', 'GET'])
+def analytics_show(rule):
+    if not session.get('logged_in'):
+        return redirect(url_for('routes.home'))
+    else:
+        userdata = {
+            'fullname': session.get('name'),
+            'email': session.get('email'),
+            'user_id': session.get('user_id')
+        }
+
+        # Default finish and start times
+        finish = datetime.date.today()
+        start = finish - datetime.timedelta(days=7)
+
+        # Override finish and start times if set
+        if request.method == 'POST':
+            if request.form['start']:
+                start = datetime.datetime.strptime(
+                    request.form['start'], '%d/%m/%Y'
+                )
+            if request.form['finish']:
+                finish = datetime.datetime.strptime(
+                    request.form['finish'], '%d/%m/%Y'
+                )
+
+        # This will validate that the rule belongs to the user and exists
+        db = get_db_session()
+        query = db.query(Rule).\
+            filter(and_(Rule.id == rule, Rule.userid == userdata['user_id']))
+
+        try:
+            rule = query.one()
+        except NoResultFound:
+            return error_page("Invalid Rule ID")
+
+        query = db.query(
+            func.date(Tweet.timestamp).label('date'),
+            Tweet.sentiment,
+            func.count(Tweet.sentiment).label('qty')
+        ).filter(
+            Tweet.timestamp > start,
+            Tweet.timestamp < finish,
+            Tweet.rule == rule
+        ).group_by(
+            func.date(Tweet.timestamp),
+            Tweet.sentiment
+        ).order_by(
+            func.date(Tweet.timestamp)
+        )
+
+        analyticdata = query.all()
+
+        resultarray = OrderedDict()
+        for result in analyticdata:
+            if result.date not in resultarray:
+                resultarray[result.date] = {}
+            resultarray[result.date][result.sentiment] = int(result.qty)
+
+        returnresult = OrderedDict()
+        for result_date, sentiment_dict in resultarray.iteritems():
+            returnresult[result_date] = {0: 0, 1: 0, 2: 0}
+            tsum = count = 0
+            for key, value in sentiment_dict.iteritems():
+                tsum += int(value)
+                count += 1
+
+            for sentiment, value in sentiment_dict.iteritems():
+                returnresult[result_date][sentiment] = (
+                    float(value) / tsum
+                ) * 100
+
+        local_config = {
+            "page_name": "Analytics for %s" % (rule.rulename)
+        }
+
+        return render_template(
+            'user_analytics.html',
+            global_config=CONFIG,
+            local_config=local_config,
+            userdata=userdata,
+            start=start,
+            finish=finish,
+            ruledata=rule,
+            menu_rules=generate_menu_items(),
+            analyticdata=returnresult
+        )
+
+
 @bp.route('/monitor/rule/<path:rule>', methods=['POST'])
 def monitor_rule_post(rule):
     if not session.get('logged_in'):
@@ -217,6 +316,7 @@ def monitor_rule_post(rule):
             userdata=userdata,
             ruledata=rule,
             success=success,
+            menu_rules=generate_menu_items(),
             error=error
         )
 
@@ -256,6 +356,7 @@ def monitor_rule(rule):
             userdata=userdata,
             ruledata=rule,
             success="",
+            menu_rules=generate_menu_items(),
             error={"errors": 0, "message": ""}
         )
 
@@ -314,6 +415,7 @@ def monitor_add():
             userdata=userdata,
             result=result,
             success=success,
+            menu_rules=generate_menu_items(),
             error=error
         )
 
@@ -356,6 +458,7 @@ def monitor():
             global_config=CONFIG,
             local_config=local_config,
             userdata=userdata,
+            menu_rules=generate_menu_items(),
             rules=rules
         )
 
@@ -387,8 +490,28 @@ def freetext():
             local_config=local_config,
             userdata=userdata,
             result=result,
+            menu_rules=generate_menu_items(),
             freetext=freetext
         )
+
+
+def generate_menu_items():
+    db = get_db_session()
+    query = db.query(
+        Rule.rulename,
+        Rule.id
+    ).filter(
+        Rule.userid == session.get('user_id'),
+        Rule.active == 1
+    ).order_by(
+        Rule.rulename
+    )
+    try:
+        rules = query.all()
+    except NoResultFound:
+        rules = []
+
+    return rules
 
 
 def error_page(message):
